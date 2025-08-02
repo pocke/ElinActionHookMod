4bb9d1272865ea0ed8436f2779e8b0e8a7331109
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2025-08-02 23:02:39 +0900

    Tweak indentation

--- ActionHook.cs
+++ ActionHook.cs
@@ -8,39 +8,39 @@ namespace ActionHook;
 
 internal static class ModInfo
 {
-    internal const string Guid = "me.pocke.action-hook";
-    internal const string Name = "Action Hook";
-    internal const string Version = "1.0.0";
+  internal const string Guid = "me.pocke.action-hook";
+  internal const string Name = "Action Hook";
+  internal const string Version = "1.0.0";
 }
 
 [BepInPlugin(ModInfo.Guid, ModInfo.Name, ModInfo.Version)]
 internal class ActionHook : BaseUnityPlugin
 {
-    internal static ActionHook Instance { get; private set; }
+  internal static ActionHook Instance { get; private set; }
 
-    Dictionary<Events.EventBase, List<Actions.ActionBase>> Actions { get; set; } = null;
+  Dictionary<Events.EventBase, List<Actions.ActionBase>> Actions { get; set; } = null;
 
-    public void Awake()
+  public void Awake()
+  {
+    Instance = this;
+    new Harmony(ModInfo.Guid).PatchAll();
+
+    Actions = ConfigLoader.LoadActionsFromCsv(ConfigLoader.ConfigPath);
+  }
+
+  public static void Log(object message)
+  {
+    Instance.Logger.LogInfo(message);
+  }
+
+  public static void Call(Events.EventBase ev)
+  {
+    if (Instance.Actions.TryGetValue(ev, out var actions))
     {
-        Instance = this;
-        new Harmony(ModInfo.Guid).PatchAll();
-
-        Actions = ConfigLoader.LoadActionsFromCsv(ConfigLoader.ConfigPath);
+      foreach (var action in actions)
+      {
+        action.Do(ev);
+      }
     }
-
-    public static void Log(object message)
-    {
-        Instance.Logger.LogInfo(message);
-    }
-
-    public static void Call(Events.EventBase ev)
-    {
-        if (Instance.Actions.TryGetValue(ev, out var actions))
-        {
-            foreach (var action in actions)
-            {
-                action.Do(ev);
-            }
-        }
-    }
+  }
 }
--- Patch.cs
+++ Patch.cs
@@ -6,42 +6,43 @@ namespace ActionHook;
 [HarmonyPatch]
 public static class Patch
 {
-    [HarmonyPrefix, HarmonyPatch(typeof(Player), nameof(Player.EnterLocalZone), new Type[] { typeof(Point), typeof(ZoneTransition), typeof(bool), typeof(Chara) })]
-    public static void Player_EnterLocalZone_Prefix(Point p, out Events.SubType? __state)
+  [HarmonyPrefix, HarmonyPatch(typeof(Player), nameof(Player.EnterLocalZone), new Type[] { typeof(Point), typeof(ZoneTransition), typeof(bool), typeof(Chara) })]
+  public static void Player_EnterLocalZone_Prefix(Point p, out Events.SubType? __state)
+  {
+    p = p.Copy();
+    if (EClass._zone.IsRegion)
     {
-        p = p.Copy();
-        if (EClass._zone.IsRegion)
-        {
-            p.Set(p.x + EClass.scene.elomap.minX, p.z + EClass.scene.elomap.minY);
-        }
-
-        Zone zone = EClass._zone.Region.GetZoneAt(p.x, p.z);
-        __state = null;
-
-        if (zone is Zone_Dungeon)
-        {
-            __state = Events.SubType.Nefia;
-        }
-        else if (zone != null && zone.IsPCFaction) {
-            __state = Events.SubType.PCFaction;
-        }
-        else
-        {
-            return;
-        }
-
-        var ev = new Events.EnterZone { SubType = __state, Phase = Events.Phase.Before };
-        ActionHook.Call(ev);
+      p.Set(p.x + EClass.scene.elomap.minX, p.z + EClass.scene.elomap.minY);
     }
 
-    [HarmonyPostfix, HarmonyPatch(typeof(Player), nameof(Player.EnterLocalZone), new Type[] { typeof(Point), typeof(ZoneTransition), typeof(bool), typeof(Chara) })]
-    public static void Player_EnterLocalZone_Postfix(Point p, Events.SubType? __state)
+    Zone zone = EClass._zone.Region.GetZoneAt(p.x, p.z);
+    __state = null;
+
+    if (zone is Zone_Dungeon)
     {
-        if (__state == null)
-        {
-            return;
-        }
-        var ev = new Events.EnterZone { SubType = __state, Phase = Events.Phase.After };
-        ActionHook.Call(ev);
+      __state = Events.SubType.Nefia;
     }
+    else if (zone != null && zone.IsPCFaction)
+    {
+      __state = Events.SubType.PCFaction;
+    }
+    else
+    {
+      return;
+    }
+
+    var ev = new Events.EnterZone { SubType = __state, Phase = Events.Phase.Before };
+    ActionHook.Call(ev);
+  }
+
+  [HarmonyPostfix, HarmonyPatch(typeof(Player), nameof(Player.EnterLocalZone), new Type[] { typeof(Point), typeof(ZoneTransition), typeof(bool), typeof(Chara) })]
+  public static void Player_EnterLocalZone_Postfix(Point p, Events.SubType? __state)
+  {
+    if (__state == null)
+    {
+      return;
+    }
+    var ev = new Events.EnterZone { SubType = __state, Phase = Events.Phase.After };
+    ActionHook.Call(ev);
+  }
 }
